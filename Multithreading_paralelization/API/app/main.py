3bc69e5d8a747b2cb41fb6a6a05b5504039db35c
Antoine DESRUET <antoine.desruet@epitech.eu>
2025-04-06 23:05:09 +0200

    feat: dockerfiles

---
+++ Multithreading_paralelization/API/app/main.py
@@ -0,0 +1,29 @@
+import random
+import time
+from typing import List
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class MeterConso(BaseModel):
+    meter_id: int
+    consommation: int
+    unit: str
+
+
+METER_CONSO: List[MeterConso] = [
+    MeterConso(meter_id=i, consommation=random.randint(0, 10000), unit="W")
+    for i in range(200)
+]
+
+
+@app.get("/meter/{meter_id}/conso")
+def get_meter_consommation(meter_id: int) -> MeterConso:
+    try:
+        meter: MeterConso = METER_CONSO[meter_id]
+        time.sleep(random.randint(1, 10) / 100)
+        return meter
+    except IndexError:
+        raise HTTPException(status_code=404, detail="Item not found")
