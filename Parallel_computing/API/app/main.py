418ac25d3c1dade288f7636045c432d8ead71927
Antoine DESRUET <antoine.desruet@epitech.eu>
2025-04-14 23:25:30 +0200

    oui

--- Parallel_computing/API/app/main.py
+++ Parallel_computing/API/app/main.py
@@ -18,12 +18,14 @@ METER_CONSO: List[MeterConso] = [
     for i in range(200)
 ]
 
+SLEEP_TIME: List[float] = [random.randint(1, 5) / 100 for _ in range(200)]
+
 
 @app.get("/meter/{meter_id}/conso")
 def get_meter_consommation(meter_id: int) -> MeterConso:
     try:
         meter: MeterConso = METER_CONSO[meter_id]
-        time.sleep(random.randint(1, 10) / 100)
+        time.sleep(SLEEP_TIME[meter_id])
         return meter
     except IndexError:
         raise HTTPException(status_code=404, detail="Item not found")
