44d723e27adadbf0e640e8dab536d70c5daeab0e
Antoine DESRUET <antoine.desruet@epitech.eu>
2025-07-06 23:36:36 +0200

    feat: db connection in context

--- GraphQL_API/app/routes/graphql/users.py
+++ GraphQL_API/app/routes/graphql/users.py
@@ -1,10 +1,19 @@
 from typing import List
 
 import strawberry
+from fastapi import Depends
 from strawberry.fastapi import GraphQLRouter
 
 from app.controllers.user import get_all_users, get_user_by_id
 from app.database import get_db
+
+
+async def get_context(
+    db_con=Depends(get_db),
+):
+    return {
+        "db": db_con,
+    }
 
 
 @strawberry.type
@@ -14,15 +23,17 @@ class User:
     firstname: str
 
     @strawberry.field
-    def first_user(root) -> "User":
-        return User(id=0, name="OUI", firstname="NON")  # type: ignore
+    def first_user(root, info: strawberry.Info) -> "User":
+        db = info.context["db"]
+        user = get_user_by_id(int(root.id), db)
+        return User(id=user.id, name=user.name, firstname=user.firstname)  # type: ignore
 
 
 @strawberry.type
 class Query:
     @strawberry.field
-    def users(self) -> List[User]:
-        db = next(get_db())
+    def users(self, info: strawberry.Info) -> List[User]:
+        db = info.context["db"]
 
         return [
             User(id=user.id, name=user.name, firstname=user.firstname)  # type: ignore
@@ -30,10 +41,12 @@ class Query:
         ]
 
     @strawberry.field
-    def user(self, id: int) -> User:
-        db = next(get_db())
+    def user(self, id: int, info: strawberry.Info) -> User:
+        db = info.context["db"]
         user = get_user_by_id(id, db)
         return User(id=user.id, name=user.name, firstname=user.firstname)  # type: ignore
 
 
-router = GraphQLRouter(schema=strawberry.Schema(query=Query))
+router = GraphQLRouter(
+    schema=strawberry.Schema(query=Query), context_getter=get_context
+)
